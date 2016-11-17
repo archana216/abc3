e485a0949450367634065032a4caeb2fa170c348
Rushikesh Gaidhani <rushi.rg@gmail.com>
2016-11-17 12:54:04 +0530

    changed array to dynamic
    dynamic array has been created

--- buuble.c
+++ buuble.c
@@ -2,19 +2,21 @@
  
 int main()
 {
-  int array[100], n, c, d, swap;
+  int n, no, c, d, swap;
  
   printf("Enter number of elements\n");
   scanf("%d", &n);
  
-  printf("Enter %d integers\n", n);
+ int *array = (int*)malloc(sizeof(int)*n);
  
-  for (c = 0; c < n; c++)
+  printf("Enter %d integers\n", no);
+ 
+  for (c = 0; c < no; c++)
     scanf("%d", &array[c]);
  
-  for (c = 0 ; c < ( n - 1 ); c++)
+  for (c = 0 ; c < ( no - 1 ); c++)
   {
-    for (d = 0 ; d < n - c - 1; d++)
+    for (d = 0 ; d < no - c - 1; d++)
     {
       if (array[d] > array[d+1]) /* For decreasing order use < */
       {
